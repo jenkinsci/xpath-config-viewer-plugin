5fd91ecd695aba6d0335714d9a1b8c4146d8fae7
Frederik Fromm <frederik.fromm@gmail.com>
2012-03-06 19:20:59 +0100

    added sort by config id.

---
+++ src/main/java/org/jenkinsci/plugins/viewer/XPathConfigComparator.java
@@ -0,0 +1,11 @@
+package org.jenkinsci.plugins.viewer;
+
+/**
+ * Created by IntelliJ IDEA.
+ * User: jet
+ * Date: 3/6/12
+ * Time: 6:04 PM
+ * To change this template use File | Settings | File Templates.
+ */
+public class XPathConfigComparator {
+}
---
+++ src/test/java/org/jenkinsci/plugins/viewer/XPathConfigProviderTest.java
@@ -0,0 +1,16 @@
+package org.jenkinsci.plugins.viewer;
+
+import junit.framework.TestCase;
+
+/**
+ * Created by IntelliJ IDEA.
+ * User: jet
+ * Date: 3/6/12
+ * Time: 6:37 PM
+ * To change this template use File | Settings | File Templates.
+ */
+public class XPathConfigProviderTest extends TestCase {
+    public void testGetAllConfigs() throws Exception {
+
+    }
+}
